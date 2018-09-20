cc031632cab5bacd6d386566b2b9c1809714c33b
Maxim Sambulat <maxim.sambulat@gmail.com>
2018-09-20 14:55:48 +0300

    Removed Eureka client

--- src/main/kotlin/com/procurement/point/PublicPointApplication.kt
+++ src/main/kotlin/com/procurement/point/PublicPointApplication.kt
@@ -3,10 +3,8 @@ package com.procurement.point
 import com.procurement.point.config.ApplicationConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication(scanBasePackageClasses = [ApplicationConfig::class])
-@EnableEurekaClient
 class PublicPointApplication
 
 fun main(args: Array<String>) {
