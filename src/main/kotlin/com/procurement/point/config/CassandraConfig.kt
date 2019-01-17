ca05e05a157fb7813c15f5899f175506a3fbd6cc
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-01-17 11:21:11 +0200

    Added Cassandra health check

--- src/main/kotlin/com/procurement/point/config/CassandraConfig.kt
+++ src/main/kotlin/com/procurement/point/config/CassandraConfig.kt
@@ -1,5 +1,9 @@
 package com.procurement.point.config
 
+import com.datastax.driver.core.Session
+import com.procurement.point.infrastructure.metric.CassandraHealthIndicator
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories
@@ -7,4 +11,9 @@ import org.springframework.data.cassandra.repository.config.EnableCassandraRepos
 @Configuration
 @ComponentScan(basePackages = ["com.procurement.point.model.entity"])
 @EnableCassandraRepositories(basePackages = ["com.procurement.point.repository"])
-class CassandraConfig
+class CassandraConfig(private val session: Session) {
+    @Bean
+    fun cassandraHealthIndicator(): HealthIndicator {
+        return CassandraHealthIndicator(session = session)
+    }
+}
